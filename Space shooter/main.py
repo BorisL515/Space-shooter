b7062aa05bceaff5d4f3b4f07290c37da6bf7560
Borisaurus <116396879+BorisL515@users.noreply.github.com>
2023-09-28 22:04:37 -0300

    Add files via upload

--- Space shooter/main.py
+++ Space shooter/main.py
@@ -10,16 +10,20 @@ icon_path = os.path.join("Models", "Icon.png")
 icon = pygame.image.load(icon_path)
 pygame.display.set_icon(icon)
 
+Player_path = os.path.join("Models", "Player.png")
 Background_path = os.path.join("Models", "Background.png")
 Background = pygame.image.load(Background_path)
-
+Playerimg = pygame.image.load(Player_path)
 running = True
 
-    
+def player():
+    screen.blit(Playerimg,(370,480))
 
 while running:
     screen.blit(Background, (0,0))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running = False
+    player()        
     pygame.display.update()
+
